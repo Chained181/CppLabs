d63571a6790763ed2910be3cf772661b66b2c521
Chained181 <85314212+Chained181@users.noreply.github.com>
2021-09-29 22:04:09 +0300

    Add files via upload

---
+++ FirstLab/SecondQuestion/SecondQuestion/Header.h
@@ -0,0 +1,5 @@
+void initialization_array_2D(int** arr, int nRows, int nCols);
+int factorial(int x);
+void print_array_2D(int** arr, int nRows, int nCols);
+int* array_1D(int** arr, int SIZE, int N, int M);
+void print_array_1D(int* arr1D, unsigned int SIZE);
---
+++ FirstLab/SecondQuestion/SecondQuestion/SecondQuestion.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#include "Header.h"
+#include "SecondQuestion.h"
+
+void main()
+{
+    const unsigned int nRows = 5;
+    const unsigned int nCols = 5;
+
+    int** aArr2D = new int* [nRows];
+    for (int i = 0; i < nRows; i++)
+    {
+        *(aArr2D + i) = new int[nCols];
+    }
+
+    initialization_array_2D(aArr2D, nRows, nCols);
+
+    int factorial(int x);
+
+    print_array_2D(aArr2D, nRows, nCols);
+
+    const unsigned int SIZE = 15;
+
+    int* arr1D = array_1D(aArr2D, SIZE, nRows, nCols);
+
+    print_array_1D(arr1D, SIZE);
+
+    cin.get();
+}
