763daa63b2573e5414180f75873da19a49e23777
Chained181 <85314212+Chained181@users.noreply.github.com>
2021-09-29 22:08:26 +0300

    Add files via upload

---
+++ FirstLab/FirstQuestion/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <random>
+#include <time.h>
+#include <cmath>
+using namespace std;
+
+void processArray() {
+	srand(time(0));
+	float Array[20] = {};
+	int maxMassElement = Array[0];
+	float delArray[20] = {};
+	
+	float x;
+	float a = 1;
+	for (int i = 0; i < 20; i++) 
+	{
+		Array[i] = -20 + rand() % +90;
+		cout<< Array[i] << " ";
+		
+		if (Array[i] > maxMassElement) {
+			maxMassElement = Array[i];
+		}
+		
+	}
+	cout<< " \n";
+	cout << "Max mass Element = " << maxMassElement << "\n";
+	for (int i = 0; i < 20; i++) {
+		delArray[i] = Array[i] / abs(maxMassElement);
+		cout << delArray[i] << " ";
+	}
+	cout << "\n";
+	cout << "Put number X"<< "\n";
+	cin >> x;
+	cout << "\n";
+
+	for (int i = 0; i < 20; i++) {
+		
+		if (delArray[i] == a) {
+			delArray[i] = x;
+			
+			for (int i = 0; i < 20; i++) {
+				delArray[i] = x;
+			}
+			cout << delArray[i] << " ";
+		}
+		else
+		{
+			cout << delArray[i] << " ";
+		}
+		
+		
+	}
+	
+	
+}
+
+int main() {
+	processArray();
+}
